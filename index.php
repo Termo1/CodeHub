ed27ce1b08703f0a1775392b650ac1f847e1e7d9
Termo1 <adamburansky9@gmail.com>
2025-03-28 13:24:19 +0100

    idk

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href = "css/style.css">
-    
-    
+    <?php require_once("parts/head.php")?>
     <title>CodeHub</title>
 </head>
 <body>
